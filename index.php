1738eea39ab2087a8f9640f9e83c387fe6f4f486
DrRicx <143243737+DrRicx@users.noreply.github.com>
2023-11-18 12:05:40 +0800

    add nav and hero section

--- index.php
+++ index.php
@@ -5,11 +5,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>MindLoom</title>
     <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
     <script src="main.js"></script>
 </head>
 <body>
-
+<section class="navigation-bar">
+    <nav class=navbar>
+        <div class="brand-div">
+            <p class="brand-name"><a href="index.php">MindLoom</a></p>
+        </div>
+        <div class="nav-options">
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Test</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+    </nav>
+</section>
+<section class="hero">
+</section>
 </body>
 </html>
