9d86487e9b2e0239b807f57767034fe7d6bf99b0
DrRicx <143243737+DrRicx@users.noreply.github.com>
2023-11-20 19:12:30 +0800

    added hero section and who we help

--- index.php
+++ index.php
@@ -26,6 +26,40 @@
     </nav>
 </section>
 <section class="hero">
+    <div class="hero-contents">
+        <div class="hero-image"><img src="res/hero-pics/hero_image.png" alt="hero_image" height="500px" width="500px">
+        </div>
+        <div class="hero-textContent">
+            <div class="textTitle"><p>Insert Title</p></div>
+            <div class="textDescription"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis
+                    corporis dolorum eius, hic illum in molestiae nesciunt numquam optio pariatur porro praesentium quae
+                    quos reiciendis repudiandae rerum sapiente tenetur totam voluptas voluptatem voluptates voluptatum?
+                    Culpa debitis dolor eligendi error fugiat inventore ipsum, iste natus necessitatibus nulla possimus
+                    quaerat, quibusdam, recusandae repellat reprehenderit sunt suscipit?</p></div>
+            <div>
+                <a href="#" class="hero-button">Join Now</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="firstBar">
+    <div class="WWH-title">
+        <p>Who We Help?</p>
+    </div>
+    <div class="barPicturesContainer">
+        <div class="firstPicture">
+            <img src="res/firstBar-pics/1.jpg" alt="1" height="220px" width="220px">
+            <p>Nathan1</p>
+        </div>
+        <div class="secondPicture">
+            <img src="res/firstBar-pics/1.jpg" alt="1" height="220px" width="220px">
+            <p>Nathan2</p>
+        </div>
+        <div class="thirdPicture">
+            <img src="res/firstBar-pics/1.jpg" alt="1" height="220px" width="220px">
+            <p>Nathan3</p>
+        </div>
+    </div>
 </section>
 </body>
 </html>
